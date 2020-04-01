7f0037af8ee61aafe767ba69781b5e74e155031b
Hisoka2000 <gamewalkthrougher@gmail.com>
2020-04-01 17:36:10 +0400

    Initial Commit

---
+++ Project02/Final/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class EntryPoint
+    {
+        public static void Main()
+        {
+            System.Console.Out.WriteLine("Hello World");
+
+
+        #if DEBUG
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        #endif
+        }
+    }
+}
